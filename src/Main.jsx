6d09520a660a4f6b4bd0ed40d11948d2ef03912e
Drashti <72065999+Drashti-29@users.noreply.github.com>
2020-12-30 21:58:56 +0530

    cart

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import App from "./components/App";
 import SignUp from "./components/SignUp";
+// import Home from "./components/Home";
+
 export default function Main() {
   return (
     <>
       <Switch>
         <Route path="/login" component={App} />
         <Route path="/signup" component={SignUp} />
+        {/* <Route path="/home" component={Home} /> */}
+
         <Redirect to="/login" />
       </Switch>
     </>
